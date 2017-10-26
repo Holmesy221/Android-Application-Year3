33f47bd6afb0f66b13bd721a37fd754c77bbe770
Holmesy221 <holmesy221@gmail.com>
2017-10-26 18:33:34 +0100

    update

--- mobile/src/main/java/covuni/a300cemcovunistudenthelper/TimeLineFragment.java
+++ mobile/src/main/java/covuni/a300cemcovunistudenthelper/TimeLineFragment.java
@@ -20,6 +20,7 @@ import com.twitter.sdk.android.tweetui.UserTimeline;
 
 public class TimeLineFragment extends ListFragment {
     //Put together by using official Fabric Doc. HERE: https://dev.twitter.com/twitterkit/android/show-timelines
+    //and https://dev.twitter.com/twitterkit/android/installation
 
 
     //TODO: Change to recycler view.
