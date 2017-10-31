f3a2f94765ec6cf116fa3f803588288d59b528b6
Holmesy221 <holmesy221@gmail.com>
2017-10-31 09:57:04 +0000

    implemented cardView - finished populating.

--- mobile/src/main/java/covuni/a300cemcovunistudenthelper/BuildingList.java
+++ mobile/src/main/java/covuni/a300cemcovunistudenthelper/BuildingList.java
@@ -11,7 +11,27 @@ public class BuildingList {
 
     public static final BuildingList[] buildings = {
             new BuildingList("Alan Berry", R.drawable.alan_berry),
-            new BuildingList("Alan Berry", R.drawable.alan_berry)
+            new BuildingList("Alma ", R.drawable.alma_building),
+            new BuildingList("Armstrong Siddeley", R.drawable.armstrong_siddeley),
+            new BuildingList("Charles Ward", R.drawable.bugatti),
+            new BuildingList("Engineering and Computing", R.drawable.ec_building),
+            new BuildingList("Ellen Terry", R.drawable.ellen_terry),
+            new BuildingList("Frederick Lancaster Library", R.drawable.lanchester_library),
+            new BuildingList("George Eliot", R.drawable.george_eliot),
+            new BuildingList("Graham Sutherland", R.drawable.graham_sutherland),
+            new BuildingList("The Hub", R.drawable.the_hub),
+            new BuildingList("Jaguar", R.drawable.jaguar_building),
+            new BuildingList("James Starley", R.drawable.james_starley),
+            new BuildingList("Maurice Foss", R.drawable.maurice_foss),
+            new BuildingList("Multi Story CarPark", R.drawable.multi),
+            new BuildingList("Priory Building", R.drawable.priory_building),
+            new BuildingList("Richard Crossman", R.drawable.richard_crossman),
+            new BuildingList("Sir John Laing", R.drawable.sir_john_laing),
+            new BuildingList("Sir William Lyons", R.drawable.sir_william_lyons),
+            new BuildingList("Sports Center", R.drawable.sport_centre),
+            new BuildingList("Student Center", R.drawable.student_centre)
+
+
 
     };
 
