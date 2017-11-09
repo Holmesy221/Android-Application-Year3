5be33f7f101e1e0bb648cc9c97e331b3373b1bf1
Holmesy221 <holmesy221@gmail.com>
2017-11-09 18:56:35 +0000

    added images for pubs n clubs

--- mobile/src/main/java/covuni/a300cemcovunistudenthelper/PubsClubsList.java
+++ mobile/src/main/java/covuni/a300cemcovunistudenthelper/PubsClubsList.java
@@ -12,20 +12,20 @@ public class PubsClubsList {
 
     public static final PubsClubsList[] pubBuildings = {
             new PubsClubsList("Cosmos", R.drawable.cosmos),
-            new PubsClubsList("Slug and Lettuce",R.drawable.alan_berry),
-            new PubsClubsList("Flying Standard", R.drawable.alan_berry),
-            new PubsClubsList("Nandos",R.drawable.alan_berry),
-            new PubsClubsList("Hans Dyansty",R.drawable.alan_berry),
-            new PubsClubsList("The Earl of Mercia", R.drawable.alan_berry),
-            new PubsClubsList("Drapers Bar and Kitchen", R.drawable.alan_berry),
-            new PubsClubsList("Ivy House", R.drawable.alan_berry),
-            new PubsClubsList("Pheonix Bar", R.drawable.alan_berry),
-            new PubsClubsList("Quidds inn", R.drawable.alan_berry),
-            new PubsClubsList("Kasbah Nightclub",R.drawable.alan_berry),
-            new PubsClubsList("Town Wall Tavern",R.drawable.alan_berry),
-            new PubsClubsList("The Artisian Bar and Grill", R.drawable.alan_berry),
-            new PubsClubsList("The Town Crier", R.drawable.alan_berry),
-            new PubsClubsList("The Oak Inn", R.drawable.alan_berry)
+            new PubsClubsList("Slug and Lettuce",R.drawable.slug_lettuce),
+            new PubsClubsList("Flying Standard", R.drawable.the_flying_standard),
+            new PubsClubsList("Nandos",R.drawable.nandos),
+            new PubsClubsList("Hans Dyansty",R.drawable.hans_dyansty),
+            new PubsClubsList("The Earl of Mercia", R.drawable.the_earl_of_mercia),
+            new PubsClubsList("Drapers Bar and Kitchen", R.drawable.drapers),
+            new PubsClubsList("Ivy House", R.drawable.ivy_house),
+            new PubsClubsList("Pheonix Bar", R.drawable.phoenix_bar),
+            new PubsClubsList("Quidds inn", R.drawable.quidds_inn),
+            new PubsClubsList("Kasbah Nightclub",R.drawable.kasbah),
+            new PubsClubsList("Town Wall Tavern",R.drawable.town_wall_tavern),
+            new PubsClubsList("The Artisian Bar and Grill", R.drawable.artisan),
+            new PubsClubsList("The Town Crier", R.drawable.town_crier),
+            new PubsClubsList("The Oak Inn", R.drawable.theoak)
 
     };
 
